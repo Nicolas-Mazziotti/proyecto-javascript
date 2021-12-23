43b8c39f243abf452e54a12c181345754a0693f3
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2021-12-23 13:18:45 -0300

    ajax

--- accesorios.js
+++ accesorios.js
@@ -1,34 +1,16 @@
+//Cards ajax
 
 const URL = "http://127.0.0.1:5500/accesorios.json";
 
 $.get(URL, function (respuesta,estado){
+    console.log(respuesta)
     if(estado == "success"){
         let misDatos = respuesta
-        for (const datos of misDatos){
-            $("#carousel-accesorios").append(`
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img src="${datos.img}" class="d-block w-25 " alt="...">
-                </div>
-                <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-                </div>
-     
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-            </div>
-            `)
+        for (const dato of misDatos){
+                $("#cards-accesorios").append(`
+                <img src="${dato.img}/>
+                `)
+            }
         }
     }
-})
+)
