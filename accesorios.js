591aec83b86106435ae75c5f462250fa8d5cb0a2
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2021-12-26 16:12:29 -0300

    Styles

--- accesorios.js
+++ accesorios.js
@@ -1,16 +1,22 @@
 //Cards ajax
 
-const URL = "http://127.0.0.1:5500/accesorios.json";
+const URL = "accesorios.json";
 
 $.get(URL, function (respuesta,estado){
     console.log(respuesta)
     if(estado == "success"){
         let misDatos = respuesta
-        for (const dato of misDatos){
-                $("#cards-accesorios").append(`
-                <img src="${dato.img}/>
+        for( const dato of misDatos)
+            $(".cards-accesorios").append(`
+            <div class="card m-2" style="width: 18rem;">
+            <img src="${dato.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">${dato.modelo}</p>
+            </div>
+            </div>
                 `)
-            }
+            
         }
     }
 )
+
